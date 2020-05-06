918bd330f6dfa8f5497267d23a9802fccf87c1bb
Julio <julio.vallejo@uao.edu.co>
2020-05-06 11:25:59 -0500

    Estilos y funcionalidad del popup

---
+++ resources/calendar/reserve.js
@@ -0,0 +1,6 @@
+function openReserveCard(){
+    document.getElementById("reserveCard").style.display="block";
+}
+function cancelReserveCard(){
+    document.getElementById("reserveCard").style.display="none";
+}
